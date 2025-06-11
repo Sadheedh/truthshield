8c871fe2f135f68995321a6b9d16b62aa4c813f7
Hussain Sadheedh <56693550+Sadheedh@users.noreply.github.com>
2025-06-11 20:03:34 +0800

    Updated to new model in dropbox

--- build.sh
+++ build.sh
@@ -3,8 +3,11 @@ set -e
 echo "Installing dependencies..."
 pip install -r requirements.txt
 echo "Downloading model.zip..."
-curl -L "https://dl.dropbox.com/scl/fi/zljldm6e4v94lwtlhinv9/truthshield-api.zip?rlkey=i4rekent2i9ud2bey2mfp69qs&st=s21o4ej0&dl=1" -o model.zip
+curl -L "https://dl.dropbox.com/scl/fi/yu5mtqxs3qqwe244y51qo/model.zip?rlkey=tdo9jj3yhetkf93xa4hyr8iao&st=itag7p3c&dl=1" -o model.zip
 echo "Checking file type..."
 file model.zip
 echo "Extracting model.zip..."
 unzip model.zip || { echo "Unzip failed! Check if model.zip is a valid ZIP file."; exit 1; }
+
+
+# https://dl.dropbox.com/scl/fi/yu5mtqxs3qqwe244y51qo/model.zip?rlkey=tdo9jj3yhetkf93xa4hyr8iao&st=itag7p3c&dl=1
