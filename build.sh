fcfc3cf7a985a458c47e15555c34dfb581695ae2
Hussain Sadheedh <56693550+Sadheedh@users.noreply.github.com>
2025-06-16 19:15:04 +0800

    changed model folders and new link

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@ set -e
 echo "Installing dependencies..."
 pip install -r requirements.txt
 echo "Downloading model.zip..."
-curl -L "https://dl.dropbox.com/scl/fi/fv4figkjgpuskyu8tstui/model.zip?rlkey=3e65qrupet9pufaa93c7r0zj0&st=te7ikt8z&dl=1" -o model.zip
+curl -L "https://www.dropbox.com/scl/fi/ujqlem0t4s75kqit60su5/model.zip?rlkey=rmmbge66kvdbhathwoekgqvwc&st=3bzmtylm&dl=0" -o model.zip
 echo "Checking file type..."
 file model.zip
 echo "Extracting model.zip..."
@@ -13,4 +13,4 @@ unzip model.zip || { echo "Unzip failed! Check if model.zip is a valid ZIP file.
 # https://dl.dropbox.com/scl/fi/yu5mtqxs3qqwe244y51qo/model.zip?rlkey=tdo9jj3yhetkf93xa4hyr8iao&st=itag7p3c&dl=1
 # below is old model which is named as model_old in dropbox
 # https://dl.dropbox.com/scl/fi/yu5mtqxs3qqwe244y51qo/model.zip?rlkey=tdo9jj3yhetkf93xa4hyr8iao&st=itag7p3c&dl=1
-# New one: https://dl.dropbox.com/scl/fi/fv4figkjgpuskyu8tstui/model.zip?rlkey=3e65qrupet9pufaa93c7r0zj0&st=te7ikt8z&dl=1
+# New one: https://www.dropbox.com/scl/fi/ujqlem0t4s75kqit60su5/model.zip?rlkey=rmmbge66kvdbhathwoekgqvwc&st=3bzmtylm&dl=0
